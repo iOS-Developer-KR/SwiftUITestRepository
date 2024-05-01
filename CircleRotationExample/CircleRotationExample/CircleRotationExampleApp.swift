4d328e5e33579b097a2d4d58ad07c563b252eba0
happyytw <happyytw@gmail.com>
2024-05-01 16:33:16 +0900

    add

---
+++ CircleRotationExample/CircleRotationExample/CircleRotationExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleRotationExampleApp.swift
+//  CircleRotationExample
+//
+//  Created by Taewon Yoon on 5/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CircleRotationExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
